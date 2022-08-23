b4d9477651a0b74b61d1d75dc6b99807672b1f70
joococo <2000violet@naver.com>
2022-08-23 10:58:39 +0900

    Initial commit

---
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -0,0 +1,118 @@
+package com.example.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.appopen.AppOpenAd;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.common.api.Response;
+
+import org.json.JSONObject;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mID, mPassword;
+    Button mIdSignInButton, mIdSignUpButton;
+    static RequestQueue requestQueue;
+
+    //Ad
+    private AdView mAdView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+
+        //Ads
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+
+
+
+        //여기서부터는 로그인 관련된 코드
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+
+        // Set up the login form.
+        mID = (EditText) findViewById(R.id.login_stdnum);
+        mPassword = (EditText) findViewById(R.id.login_password);
+
+        // Button
+        mIdSignInButton = (Button) findViewById(R.id.login_button); // sign in button
+        //mIdSignUpButton = (Button) findViewById(R.id.signupButton); // sign up button
+
+
+        // 로그인 버튼 클릭
+        mIdSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String userID = mID.getText().toString();
+                String userPassword = mPassword.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try{
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if(success){
+                                Toast.makeText(getApplicationContext(), "로그인에 성공했습니다.", Toast.LENGTH_SHORT).show();
+
+                                String userID = jsonResponse.getString("userID");
+                                String userPassword = jsonResponse.getString("userPassword");
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                // 로그인 하면서 사용자 정보 넘기기
+                                intent.putExtra("userID", userID);
+                                intent.putExtra("userPassword", userPassword);
+                                startActivity(intent);
+
+                            } else {
+                                Toast.makeText(getApplicationContext(), "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+                LoginRequest loginRequest = new LoginRequest(userID, userPassword, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+
+            }
+        });
+
+
+        // 회원가입 버튼 클릭
+        /**mIdSignUpButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });**/
+
+    }
+}
