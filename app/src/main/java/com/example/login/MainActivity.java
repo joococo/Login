2c883c9af18628b7433c9ed96ad8c8d0cb74417d
joococo <2000violet@naver.com>
2022-08-24 20:55:51 +0900

    only logins

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -19,13 +19,6 @@ import android.widget.Toast;
 import com.android.volley.Response;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.appopen.AppOpenAd;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.firebase.firestore.util.Listener;
 
 import org.json.JSONObject;
 
